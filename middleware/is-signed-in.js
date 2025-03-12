8839048bcb3ef3fb6d6740d594d4749dd63f8bdc
tsztin0217 <tsztin0217@gmail.com>
2025-03-12 14:32:55 -0400

    wip: defines and exports isSignedIn middleware function

---
+++ middleware/is-signed-in.js
@@ -0,0 +1,6 @@
+const isSignedIn = (req, res, next) => {
+    if (req.session.user) return next();
+    // if a user is signed in, call the next middleware function
+    // otherwise we redirect them to the sign-in page
+    res.redict('/auth/signed-in');
+}
