2eac7ae4ba567d4b33509a397e560f4f4e6e236f
tsztin0217 <tsztin0217@gmail.com>
2025-03-20 07:39:16 +0800

    Create show template for individual application details

--- controllers/application.js
+++ controllers/application.js
@@ -38,4 +38,74 @@ router.post('/', async (req, res) => {
     }
 })
 
+// GET /users/:userId/applications/:applicationId when making this request we're already here
+router.get('/:applicationId', async (req, res) => {
+    try {
+        // look up the user that's currently logged in
+        const currentUser = await User.findById(req.session.user._id);
+
+        // find the subdocument in the currently logged in user's application list
+        const application = currentUser.applications.id(req.params.applicationId);
+
+        // render a show template with the subdocument's details
+        res.render('applications/show.ejs', {
+            application 
+            // property shorthand syntax whenever 
+            // the property name and variable name holding the same value
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
+    }
+});
+
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.id(req.params.applicationId).deleteOne();
+
+        await currentUser.save() // this makes the change in the database
+
+        res.redirect(`/users/${currentUser._id}/applications`)
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+ }
+});
+
+// GET users/:userId/applications/:applicationId/edit
+// controllers/applications.js
+
+router.get('/:applicationId/edit', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      const application = currentUser.applications.id(req.params.applicationId);
+      res.render('applications/edit.ejs', {
+        application: application,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
+
+
+// PUT /users/:userId/applications/:applicationId
+// controllers/applications.js`
+
+  
+router.put('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        application.set(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications/${req.params.applicationId}`);
+
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+})
 module.exports = router;
